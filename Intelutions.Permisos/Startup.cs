b9758d98b736b882eae5936d2c9dd2396e73080b
Nudier Mena <nmena@raptortech.com>
2020-11-23 10:52:30 -0500

    Remove commented code.

--- Intelutions.Permisos/Startup.cs
+++ Intelutions.Permisos/Startup.cs
@@ -22,33 +22,8 @@ namespace Intelutions.Permisos
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
-        {
-
-            //services.Configure<CookiePolicyOptions>(options =>
-            //{
-            //    // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-            //    options.CheckConsentNeeded = context => true;
-            //    options.MinimumSameSitePolicy = SameSiteMode.None;
-            //});
-
-            //services.AddMvcCore()
-            //   .AddJsonFormatters()
-            //   .AddApiExplorer();
-
-            //services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
-            //{
-            //    builder.WithOrigins("http://localhost:8080/", "http://localhost:8081/")
-            //           .AllowAnyMethod()
-            //           .WithMethods("PUT", "DELETE", "GET", "OPTIONS")
-            //            .WithHeaders("X-Accept-Charset", "X-Accept,Content-Type", "Origin", "Content-Type", "Accept", "Authorization", "X-Requested-With", "Range", "Accept-Encoding", "Content-Length", "Access-Control-Request-Method", "Access-Control-Request-Headers")
-            //                .AllowAnyMethod()
-            //                .Build();
-
-
-            //}));
-
+        {         
             services.AddCors();
-
             services.AddMvc();
 
             services.AddDbContext<DataContext>(opt => opt.UseSqlServer(Configuration["Data:ConnectionString"]));
